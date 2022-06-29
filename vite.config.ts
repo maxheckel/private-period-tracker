8ef4ba85867fe6ca07ead88749f50f4756bf0380
Max Heckel <maxheckel@maxs-mbp.lan>
2022-06-29 15:57:03 -0400

    Remove stuff:

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: "server/public",
-  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
